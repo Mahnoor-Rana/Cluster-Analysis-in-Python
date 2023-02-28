42a7221206811454dc5035daa011fef2d2426a9c
Mahnoor Rana <mahnoorrana2002@gmail.com>
2023-02-28 23:28:20 +0500

    updated

--- Clustering in Real World/app.py
+++ Clustering in Real World/app.py
@@ -1,18 +1,10 @@
-import os
 import subprocess
+import os
+proxies = {
+    'https': 'https://52.182.8.192:3128'
+}
 
-# Set the proxy server and port
-proxy_server = '159.22.1.0' 
-proxy_port = 'aaa'
-
-# Set the program you want to open with the proxy
-program_to_open = 'SETUP.exe'
-
-# Set the arguments for the program
-program_args = ['arg1', 'arg2']
-
-# Set the environment variable for the proxy
-os.environ['http_proxy'] = f'http://{proxy_server}:{proxy_port}'
-
-# Use subprocess to open the program with the specified arguments
-subprocess.Popen([program_to_open])
+file_path = r'SETUP.exe'
+env = {**proxies, **os.environ}
+subprocess.Popen(file_path, env=env)
+print('File opened successfully')
